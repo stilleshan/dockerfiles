9853392c265bc733421c5e7da4c02afd73fb4bb5
Stille <stille@ioiox.com>
2023-02-23 17:48:37 +0800

    fix acme for x-ui-ssl

--- x-ui/x-ui-ssl/conf/acme.sh
+++ x-ui/x-ui-ssl/conf/acme.sh
@@ -17,7 +17,7 @@ acme (){
     cat /conf/account.conf >/acme.sh/account.conf
     /root/.acme.sh/acme.sh --upgrade
     /root/.acme.sh/acme.sh --register-account -m your@domain.com --server zerossl
-    /root/.acme.sh/acme.sh --issue $* --dns ${DNSAPI} -d ${DOMAIN} -d \*.${DOMAIN}
+    /root/.acme.sh/acme.sh --issue $* --keylength 2048 --dns ${DNSAPI} -d ${DOMAIN} -d \*.${DOMAIN}
     rm -rf /acme.sh/ca
     rm -rf /acme.sh/http.header
     if [ -f /acme.sh/${DOMAIN}/fullchain.cer ] && [ -f /acme.sh/${DOMAIN}/${DOMAIN}.key ]; then
